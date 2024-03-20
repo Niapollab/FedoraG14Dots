1f025d488a0a934db1edbd6c54cb5f9acc2d5efc
Niapoll <Kolya21082001@gmail.com>
2024-03-20 16:14:07 +0400

    Add add-repos.sh script

---
+++ scripts/add-repos.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -e
+
+# RPM Fusion
+sudo dnf install \
+    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+# VSCode
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/CORP_yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.CORP_repos.d/vscode.repo'
+
+CORP_REPOS=(
+    # Asus tools
+    "lukenukem/asus-linux"
+    # Bibata cursors
+    "peterwu/rendezvous"
+    # Nerd-fonts
+    "che/nerd-fonts"
+    # Nwg-look
+    "tofik/nwg-shell"
+)
+
+for repo in "${CORP_REPOS[@]}"
+do
+    sudo dnf -y copr enable "$repo"
+done
