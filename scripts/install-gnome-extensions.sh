41bb2ff64591bb67fb4bb2b59a855556806b314c
Niapoll <Kolya21082001@gmail.com>
2024-03-28 00:07:27 +0400

    Add install-gnome-extensions.sh script

---
+++ scripts/install-gnome-extensions.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+EXTENSIONS=(
+    # Clipboard manager
+    "pano@elhan.io"
+    # Blur utilities
+    "blur-my-shell@aunetx"
+    # Workspace wraparound
+    "just-perfection-desktop@just-perfection"
+)
+
+for extension in "${EXTENSIONS[@]}"
+do
+    gnome-extensions-cli install "$extension"
+done
