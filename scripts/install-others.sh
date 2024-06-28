b26d08de8f9ec7cd34ec5f9c26b73cf5e12a7c2f
Niapoll <Kolya21082001@gmail.com>
2024-06-28 13:13:11 +0400

    Add Shadowsocks client

--- scripts/install-others.sh
+++ scripts/install-others.sh
@@ -16,13 +16,22 @@ git clone 'https://github.com/joshskidmore/zsh-fzf-history-search.git' "$ZSH_CUS
 # GEF
 bash -c "$(curl -fsSL https://gef.blah.cat/sh)"
 rm -f "$HOME/.gdbinit"
-mv -f "$(find ~ -name ".gef*" | head -1)" "$HOME/.gef.py"
+mv -f "$(find ~ -name '.gef*' | head -1)" "$HOME/.gef.py"
 
 # pwndbg
 PWNDBG_URL=$(curl -sL 'https://api.github.com/repos/pwndbg/pwndbg/releases' | sed -n 's/.*\"browser_download_url\"\s*:\s*\"\(.*\)\"/\1/p' | grep '.x86_64.rpm' | head -1)
-curl -sL "$PWNDBG_URL" -o "/tmp/pwndbg.rpm"
-sudo dnf install -y "/tmp/pwndbg.rpm"
-rm -f "/tmp/pwndbg.rpm"
+curl -sL "$PWNDBG_URL" -o '/tmp/pwndbg.rpm'
+sudo dnf install -y '/tmp/pwndbg.rpm'
+rm -f '/tmp/pwndbg.rpm'
+
+# Shadowsocks client
+SHADOWSOCKS_URL="$(curl -sL 'https://api.github.com/repos/shadowsocks/shadowsocks-rust/releases' | sed -n 's/.*\"browser_download_url\"\s*:\s*\"\(.*\)\"/\1/p' | grep '.x86_64-unknown-linux-gnu.tar.xz' | head -1)"
+SHADOWSOCKS_DIR='/tmp/shadowsocks'
+mkdir -p "$SHADOWSOCKS_DIR"
+curl -sL "$SHADOWSOCKS_URL" -o "$SHADOWSOCKS_DIR/shadowsocks.tar.xz"
+tar -C "$SHADOWSOCKS_DIR" -xf "$SHADOWSOCKS_DIR/shadowsocks.tar.xz"
+install --mode=0755 --owner=root --group=root --preserve-timestamps -D --target-directory=/usr/bin "$SHADOWSOCKS_DIR/sslocal"
+rm -rf "$SHADOWSOCKS_DIR"
 
 # Install Pywalfox to Firefox
 pywalfox install
@@ -32,7 +41,7 @@ PYWALFOX_PAGE="${PYWALFOX_PAGE%%\"*}"
 
 curl -sL "$PYWALFOX_PAGE" -o '/tmp/pywalfox.xpi'
 firefox '/tmp/pywalfox.xpi'
-read -r -p "[@] Enter any key when extension will be installed"
+read -r -p '[@] Enter any key when extension will be installed'
 rm '/tmp/pywalfox.xpi'
 
 # Install ublock-origin to Firefox
@@ -41,5 +50,5 @@ UBLOCK_ORIGIN_PAGE="${UBLOCK_ORIGIN_PAGE%%\"*}"
 
 curl -sL "$UBLOCK_ORIGIN_PAGE" -o '/tmp/ublock_origin.xpi'
 firefox '/tmp/ublock_origin.xpi'
-read -r -p "[@] Enter any key when extension will be installed"
+read -r -p '[@] Enter any key when extension will be installed'
 rm '/tmp/ublock_origin.xpi'
