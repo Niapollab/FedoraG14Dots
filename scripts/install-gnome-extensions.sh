419fa2d5cf322d0e04db5a4b8a5a2c37de65c7ba
Niapoll <Kolya21082001@gmail.com>
2024-11-20 20:46:32 +0300

    Add Caffeine

--- scripts/install-gnome-extensions.sh
+++ scripts/install-gnome-extensions.sh
@@ -8,6 +8,8 @@ EXTENSIONS=(
     "blur-my-shell@aunetx"
     # Workspace wraparound
     "just-perfection-desktop@just-perfection"
+    # Stay display alive
+    "caffeine@patapon.info"
 )
 
 for extension in "${EXTENSIONS[@]}"
